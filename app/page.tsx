0fb54dd168af5413395bb249cdbee5468e7ed1c9
ShamirAli55 <shamirali9779@gmail.com>
2025-09-07 15:13:04 +0500

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,36 @@
-export default function page () {
+import { Carousel } from "@/components/carousel";
+import { Button } from "@/components/ui/button";
+import { stripe } from "@/lib/stripe"
+import Image from "next/image";
+import Link from "next/link";
+export default async function Home () {
+  const products = await stripe.products.list({
+    expand:["data.default_price"],
+    limit:5,
+  });
+  console.log(products
+  )
   return (
     <div>
-      
+      <section className="rounded bg-neutral-100 py-8 sm:py-12">
+          <div className="mx-auto grid grid-cols-1 items-center justify-center gap-8 px-8 sm:px-16">
+            <div className="max-w-md space-y-4">
+              <h2 className="text-3xl font-bold tracking-tight md:text-4xl">Welcome to My Ecommerce</h2>
+              <p className="text-neutral-600">Discover the latest products at best prices</p>
+              <Button asChild variant="default"
+              className="inline-flex items-center justify-center rounded-full px-6 py-3 bg-black text-white">
+                <Link className="inline-flex items-center justify-center rounded-full px-6 py-3"
+                href="/products">Browse All Products</Link>
+              </Button>
+            </div>
+            <Image className="rounded" 
+            src={products.data[0].images[0]} alt="Banner Image" width={450} height={450}/>
+          </div>
+      </section>
+
+      <section className="py-8">
+        <Carousel/>
+      </section>
     </div>
   )
 }
