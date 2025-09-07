1d815b8a77603a42877b69277fe82089057236ff
ShamirAli55 <shamirali9779@gmail.com>
2025-09-07 15:51:05 +0500

    components added

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default async function Home () {
   return (
     <div>
       <section className="rounded bg-neutral-100 py-8 sm:py-12">
-          <div className="mx-auto grid grid-cols-1 items-center justify-center gap-8 px-8 sm:px-16">
+          <div className="mx-auto grid grid-cols-2 items-center justify-center gap-8 px-8 sm:px-16">
             <div className="max-w-md space-y-4">
               <h2 className="text-3xl font-bold tracking-tight md:text-4xl">Welcome to My Ecommerce</h2>
               <p className="text-neutral-600">Discover the latest products at best prices</p>
@@ -24,12 +24,12 @@ export default async function Home () {
               </Button>
             </div>
             <Image className="rounded" 
-            src={products.data[0].images[0]} alt="Banner Image" width={450} height={450}/>
+            src={products.data[0].images[0]} alt="Banner Image" width={250} height={250}/>
           </div>
       </section>
 
       <section className="py-8">
-        <Carousel/>
+        <Carousel products={products.data}/>
       </section>
     </div>
   )
